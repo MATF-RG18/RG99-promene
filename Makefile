448a0068f8af00916e07fae44df1072735cb60f2
beskonacnost <kristina>
2018-11-11 00:05:29 +0100

    Prva nedelja

---
+++ Makefile
@@ -0,0 +1,16 @@
+PROGRAM = promene
+CC      = gcc
+CFLAGS  = -g -ansi -Wall -I/usr/X11R6/include -I/usr/pkg/include
+LDFLAGS = -L/usr/X11R6/lib -L/usr/pkg/lib
+LDLIBS  = -lglut -lGLU -lGL
+
+$(PROGRAM): promene.o
+	$(CC) $(LDFLAGS) -o $(PROGRAM) promene.o $(LDLIBS)
+
+.PHONY: clean dist
+
+clean:
+	-rm *.o $(PROGRAM) *core
+
+dist: clean
+	-tar -chvj -C .. -f ../$(PROGRAM).tar.bz2 $(PROGRAM)
---
+++ promene.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <GL/glut.h>
+
+static int window_width, window_height;
+
+static void on_keyboard(unsigned char key, int x, int y);
+static void on_reshape(int width, int height);
+static void on_display(void);
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+    
+    glutInitWindowSize(300, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+    
+    glutKeyboardFunc(on_keyboard);
+    glutReshapeFunc(on_reshape);
+    glutDisplayFunc(on_display);
+    
+    /*  glClearColor( 0.35,  0.35 , 0.67,0);*/
+    glClearColor(0.2,0.2,0.5,0);
+    
+    glEnable(GL_DEPTH_TEST);
+    /*glLineWidth(2);*/
+    
+    glutMainLoop();
+
+    return 0;
+}
+
+static void on_keyboard(unsigned char key, int x, int y)
+{
+    switch (key) {
+    case 27:
+        exit(0);
+        break;
+    }
+}
+
+static void on_reshape(int width, int height)
+{
+    window_width = width;
+    window_height = height;
+}
+
+static void on_display(void)
+{
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glViewport(0, 0, window_width, window_height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-1, 1, -1, 1, 1, 50);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(4, 4, 4, 0, 0, 0, 0, 0, 1);
+
+    glColor3f(200,200,0);
+    glutSolidSphere(0.5, 100, 100);
+    
+    glPushMatrix();
+    glColor3f(0.85, 0.85, 1);
+    glutWireCube(1);
+    glPopMatrix();
+       
+    glBegin(GL_LINES);
+        glColor3f(1,0,0);
+        glVertex3f(0,0,0);
+        glVertex3f(10,0,0);
+        glColor3f(0.5,0,0);
+        glVertex3f(0,0,0);
+        glVertex3f(-10,0,0);
+        
+        glColor3f(0,1,0);
+        glVertex3f(0,0,0);
+        glVertex3f(0,10,0);
+         glColor3f(0,0.5,0);
+        glVertex3f(0,0,0);
+        glVertex3f(0,-10,0);
+        
+        glColor3f(0,0,1);
+        glVertex3f(0,0,0);
+        glVertex3f(0,0,10);
+        glColor3f(0,0,0.5);
+        glVertex3f(0,0,0);
+        glVertex3f(0,0,-40);
+    glEnd();
+   
+    glutSwapBuffers();
+}
